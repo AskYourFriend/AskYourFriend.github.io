81f79f6f2d31caf547bb13fb41d0cef75935a9fd
Alex Paskal <alexpaskal@i.ua>
2016-07-26 15:10:54 +0300

    Start page

--- Content/js/view.js
+++ Content/js/view.js
@@ -1,28 +1,5 @@
 ﻿$(document).ready(function () {
-    $('#header')
-        .css('width', (document.documentElement.clientWidth - 10))
-        .css('height', 50);
-    if (document.documentElement.clientWidth >= document.documentElement.clientHeight) {
-        $('#playbutton')
-            .css('width', ((document.documentElement.clientWidth - 15) / 4))
-            .css('height', (document.documentElement.clientHeight - parseInt($('#header').css('height')) - 15));
-        $('#content')
-            .css('width', (document.documentElement.clientWidth - 15 - parseInt($('#playbutton').css('width'))))
-            .css('height', (document.documentElement.clientHeight - parseInt($('#header').css('height')) - 15));
-        $('#playbutton')
-            .css('top', parseInt($('#header').css('height')) + 5)
-            .css('left', parseInt($('#content').css('width')) + 5);
-    } else {
-        $('#playbutton')
-            .css('width', (document.documentElement.clientWidth - 10))
-            .css('height', 50);
-        $('#content')
-            .css('width', (document.documentElement.clientWidth - 10))
-            .css('height', (document.documentElement.clientHeight - parseInt($('#header').css('height')) - 20 - parseInt($('#playbutton').css('height'))));
-        $('#playbutton')
-            .css('top', (parseInt($('#header').css('height')) + parseInt($('#content').css('height')) + 10))
-            .css('left', 0);
-    }
+    SetContentCss();
     $('#content').niceScroll({
         cursorcolor: '#000000',
         cursorwidth: '6px',
@@ -31,9 +8,13 @@
     });
 });
 $(window).resize(function () {
+    SetContentCss();
+});
+
+function SetContentCss() {
     $('#header')
-        .css('width', (document.documentElement.clientWidth - 10))
-        .css('height', 50);
+    .css('width', (document.documentElement.clientWidth - 10))
+    .css('height', 50);
     if (document.documentElement.clientWidth >= document.documentElement.clientHeight) {
         $('#playbutton')
             .css('width', ((document.documentElement.clientWidth - 15) / 4))
@@ -55,4 +36,6 @@ $(window).resize(function () {
             .css('top', (parseInt($('#header').css('height')) + parseInt($('#content').css('height')) + 10))
             .css('left', 0);
     }
-});
+    $('#playtext')
+        .css('line-height', $('#playbutton').css('height'));
+}
