19fdd756222cf8f36e036d47f4bb67b788e407d7
Alex Paskal <alexpaskal@i.ua>
2016-07-30 14:42:30 +0300

    Remove Angular from start page

--- play/Content/js/app.js
+++ play/Content/js/app.js
@@ -1,14 +1,24 @@
-﻿//var tt = 123;
-//var a = {};
+﻿var a = true;
 
 var app = angular.module('App', ['firebase']);
 
 app.run(function ($rootScope, $firebaseObject) {
-    console.log('--App.run');
-    var ref = new Firebase("https://askyourfriend-6e257.firebaseio.com");
-    $rootScope.data = {};
-    var obj = $firebaseObject(ref);
-    obj.$bindTo($rootScope, 'data');
+    //console.log('--App.run');
+    //var ref = new Firebase("https://askyourfriend-6e257.firebaseio.com");
+    //$rootScope.data = {};
+    //var obj = $firebaseObject(ref);
+    //obj.$bindTo($rootScope, 'data');
+    $rootScope.cnt = '/play/play.html';
+
+    setInterval(function () {
+        a = !a;
+        if (a) {
+            $rootScope.cnt = '/play/play.html';
+        } else {
+            $rootScope.cnt = '/play/login.html';
+        }
+        $rootScope.$apply();
+    }, 2000);
 });
 
 app.controller('MainCtrl', function ($rootScope, $scope, $firebaseObject) {
