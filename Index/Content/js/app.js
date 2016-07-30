c4ffe0d35536ac23f4e78163c7ccad5601d356ee
Alex Paskal <alexpaskal@i.ua>
2016-07-30 13:58:19 +0300

    PlayDesign

---
+++ Index/Content/js/app.js
@@ -0,0 +1,16 @@
+﻿//var tt = 123;
+//var a = {};
+
+var app = angular.module('App', ['firebase']);
+
+app.run(function ($rootScope, $firebaseObject) {
+    console.log('--App.run');
+    var ref = new Firebase("https://askyourfriend-6e257.firebaseio.com");
+    $rootScope.data = {};
+    var obj = $firebaseObject(ref);
+    obj.$bindTo($rootScope, 'data');
+});
+
+app.controller('MainCtrl', function ($rootScope, $scope, $firebaseObject) {
+    
+});
