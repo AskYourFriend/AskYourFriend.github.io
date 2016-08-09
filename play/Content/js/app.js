8c06a6f2885baf7b6fc1991641059ff24c8947c1
Alex Paskal <alexpaskal@i.ua>
2016-08-09 22:02:30 +0300

    Partial views

--- play/Content/js/app.js
+++ play/Content/js/app.js
@@ -4,23 +4,38 @@ var app = angular.module('App', ['firebase']);
 
 app.run(function ($rootScope, $firebaseObject) {
     //console.log('--App.run');
-    //var ref = new Firebase("https://askyourfriend-6e257.firebaseio.com");
-    //$rootScope.data = {};
-    //var obj = $firebaseObject(ref);
-    //obj.$bindTo($rootScope, 'data');
-    $rootScope.cnt = '/play/play.html';
+    var ref = new firebase("https://askyourfriend-6e257.firebaseio.com");
+    $rootscope.data = {};
+    $rootscope.data.members = [{}];
+    var obj = $firebaseObject(ref);
+    obj.$bindTo($rootScope, 'data');
+    $rootScope.cnt = '/play/login.html';
 
-    setInterval(function () {
-        a = !a;
-        if (a) {
-            $rootScope.cnt = '/play/play.html';
-        } else {
-            $rootScope.cnt = '/play/login.html';
-        }
+    //setInterval(function () {
+    //    a = !a;
+    //    if (a) {
+    //        $rootScope.cnt = '/play/play.html';
+    //    } else {
+    //        $rootScope.cnt = '/play/login.html';
+    //    }
+    //    $rootScope.$apply();
+    //}, 2000);
+    $rootScope.$watch('data', function (newValue, oldValue) {
         $rootScope.$apply();
-    }, 2000);
+    });
 });
 
-app.controller('MainCtrl', function ($rootScope, $scope, $firebaseObject) {
-    
+app.controller('LoginCtrl', function ($rootScope, $scope, $firebaseObject) {
+    $scope.AddUser = function () {
+        $rootScope.data.members.push({ nickname: $scope.nickname });
+        $rootScope.cnt = '/play/play.html';
+    }
+});
+
+
+app.controller('PlayCtrl', function ($rootScope, $scope, $firebaseObject) {
+    $scope.DeleteGame = function () {
+        $rootScope.data.members.lenght = 0;
+        $rootScope.cnt = '/play/login.html';
+    }
 });
