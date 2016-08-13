f4d6fb9b1d834f24dd1ebbd2c97cb59d3ce6e023
Alex Paskal <alexpaskal@i.ua>
2016-08-13 14:48:22 +0300

    See statistics (withnot design)

--- Index/Content/js/app.js
+++ Index/Content/js/app.js
@@ -18,29 +18,15 @@ app.run(function ($rootScope, $firebaseObject) {
 });
 
 app.controller('MainCtrl', function ($rootScope, $scope, $firebaseObject) {
-    //$rootScope.data = { votes: [{ answers: [{}] }] };
-    //obj.$loaded(function () {
-    //    if ($rootScope.data.votes.length >= 1) {
-    //        $scope.showVote(0);
-    //    }
-    //});
     $scope.currentvoteindex = null;
     $scope.showVote = function (index) {
         $rootScope.selectedanyvote = true;
         var curr_vote = $rootScope.data.votes[index];
         $scope.name = curr_vote.name;
-        $scope.desc = curr_vote.desc;
+        $scope.question = curr_vote.question;
         $scope.nick = curr_vote.nick;
         $scope.answers = curr_vote.answers;
         $scope.currentvoteindex = index;
-        //$('[data-vote-select]').removeClass('active');
-        //console.log('remove class');
-
-        //var s = '';
-        //s += '[data-vote-select=';
-        //s += index;
-        //s += ']';
-        //$(s).addClass('active');
 
         var str = $scope.currentvoteindex.toString();
         if ($.cookie(str) != null && $.cookie(str) != 'null') {
@@ -53,21 +39,17 @@ app.controller('MainCtrl', function ($rootScope, $scope, $firebaseObject) {
     $scope.VoteClick = function (index) {
         var str = $scope.currentvoteindex.toString();
         if ($.cookie(str) == null || $.cookie(str) == 'null') {
-            //$rootScope.data.votes[currentvoteindex].answers[index].count = $rootScope.data.votes[currentvoteindex].answers[index].count + 1;
             $rootScope.data.votes[$scope.currentvoteindex].answers[index].count++;
             $.cookie(str, index.toString(), {/*expires: 366, */path: '/'});
             $scope.activeAnsw = index;
         }
         else if (parseInt($.cookie(str)) != index) {
-            //$rootScope.data.votes[currentvoteindex].answers[parseInt($.cookie(str))].count = $rootScope.data.votes[currentvoteindex].answers[parseInt($.cookie(str))].count - 1;
-            //$rootScope.data.votes[currentvoteindex].answers[index].count = $rootScope.data.votes[currentvoteindex].answers[index].count + 1;
             $rootScope.data.votes[$scope.currentvoteindex].answers[parseInt($.cookie(str))].count--;
             $rootScope.data.votes[$scope.currentvoteindex].answers[index].count++;
             $.cookie(str, index.toString(), {/*expires: 366, */path: '/' });
             $scope.activeAnsw = index;
         }
         else if (parseInt($.cookie(str)) == index) {
-            //$rootScope.data.votes[currentvoteindex].answers[index].count = $rootScope.data.votes[currentvoteindex].answers[index].count - 1;
             $rootScope.data.votes[$scope.currentvoteindex].answers[index].count--;
             $.cookie(str, null);
             $scope.activeAnsw = null;
@@ -75,3 +57,4 @@ app.controller('MainCtrl', function ($rootScope, $scope, $firebaseObject) {
         //$scope.$apply();
     }
 });
+
